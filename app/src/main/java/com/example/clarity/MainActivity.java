807d84f896405fbe61a59976a0b32edd859b5f43
Shreyash Digambar Lokhande <shreyashlokhande1224@gmail.com>
2025-02-13 01:50:40 +0530

    Login step complete :)

--- app/src/main/java/com/example/clarity/MainActivity.java
+++ app/src/main/java/com/example/clarity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.clarity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,24 +17,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DiaryDatabaseHelper dbHelper = new DiaryDatabaseHelper(this);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        Toast.makeText(this, "Starting Login Activity.", Toast.LENGTH_SHORT).show();
-        Log.d("Database","Start");
-        startActivity(intent);
 
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sharedPreferences = getSharedPreferences("logStatus",MODE_PRIVATE);
+        Boolean status = sharedPreferences.getBoolean("logStatus", false);
+
+        if (!status) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            Toast.makeText(this, "Starting Login Activity.", Toast.LENGTH_SHORT).show();
+            Log.d("Database","Start");
+
+
+            sharedPreferences.edit().putBoolean("logStatus", true).apply();
+            startActivity(intent);
+        }
 
     }
 
